c7f3e1ed2a1f8df8fa0a9a3191fe1bbba757ffbf
xxD_long <63777652+LXL334455@users.noreply.github.com>
2022-07-04 02:40:30 +0800

    Update FileTest.java

--- service/service_vod/src/test/java/com/atguigu/FileTest.java
+++ service/service_vod/src/test/java/com/atguigu/FileTest.java
@@ -18,12 +18,12 @@ public class FileTest {
     public static void main(String[] args) {
         // 1 初始化用户身份信息（secretId, secretKey）。
         // SECRETID和SECRETKEY请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-        String secretId = "AKIDud6JFuJRIrBDySgfOq3zd06vUmnX0C7l";
-        String secretKey = "OVLyKPDk0zg6F7i26qJVQ4RYNxFG0Uhd";
+        String secretId = "";
+        String secretKey = "";
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
-        Region region = new Region("ap-guangzhou");
+        Region region = new Region("");
         ClientConfig clientConfig = new ClientConfig(region);
         // 这里建议设置使用 https 协议
         // 从 5.6.54 版本开始，默认使用了 https
@@ -35,7 +35,7 @@ public class FileTest {
             // 指定要上传的文件
             File localFile = new File("E:\\xiaohuangya.png");
             // 指定文件将要存放的存储桶
-            String bucketName = "pic-1252361084";
+            String bucketName = "";
             // 指定文件上传到 COS 上的路径，即对象键。例如对象键为folder/picture.jpg，则表示将文件 picture.jpg 上传到 folder 路径下
             String key = "images/xiaohuangya.png";
             PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
